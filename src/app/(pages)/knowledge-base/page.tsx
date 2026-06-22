c7cee49d82e8f16ffc7f9ffef2992f0f74c4144a
Zeyadhatem391 <zeyadhatemsabry@gmail.com>
2026-06-22 21:09:53 +0300

    add knowledge base

---
+++ src/app/(pages)/knowledge-base/page.tsx
@@ -0,0 +1,10 @@
+import DashbordLayout from "@/components/layout/DashbordLayout";
+import AddressSection from "@/components/molecules/AddressSection";
+
+export default function KnowledgePage() {
+  return (
+    <DashbordLayout>
+      <AddressSection titel="Knowledge Base" />
+    </DashbordLayout>
+  );
+}
